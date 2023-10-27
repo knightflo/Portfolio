56c517fd1e779af5b2d76c2e26361e50e749dece
knightflo <f.van.den.berg@sintlucasedu.nl>
2023-10-27 13:42:38 +0200

    finished the website
    yay

---
+++ js/verander.js
@@ -0,0 +1,103 @@
+
+
+
+function games(){
+    document.getElementById("image1").style.backgroundImage = 'url("img/point and click.png")';
+    document.getElementById("image2").style.backgroundImage = 'url("img/pong.png")';
+    document.getElementById("image3").style.backgroundImage = 'url("img/troll.png")';
+    document.getElementById("image4").style.backgroundImage = 'url("img/Troll (2).png")';
+    document.getElementById("image5").style.backgroundImage = 'url("img/snail.png")';
+
+    document.getElementById("title1").innerText = "Point en click game";
+    document.getElementById("title2").innerText = "Pong";
+    document.getElementById("title3").innerText = "Troll game for mobile";
+    document.getElementById("title4").innerText = "Trolly adventures";
+    document.getElementById("title5").innerText = "The snail foundation";
+
+    document.getElementById("tags1").innerHTML = "<p>HTML</p>\n"+"<p>CSS</p>\n"+"<p>Javascript</p>";
+    document.getElementById("tags2").innerHTML = "<p>C#</p>\n" +
+        "                        <p>Unity</p>";
+    document.getElementById("tags3").innerHTML = "<p>C#</p>\n" +
+        "                        <p>Unity</p>";
+    document.getElementById("tags4").innerHTML = "<p>Stencyl</p>";
+    document.getElementById("tags5").innerHTML = "<p>C#</p>"+ "<p>Unity</p>";
+
+    document.getElementById("description1").innerText = "Een game, gemaakt in een website, waarin je moet aanwijzen en klikken om voort te bewegen";
+    document.getElementById("description2").innerText = "Het bekende tafeltennis spel op het scherm";
+    document.getElementById("description3").innerText = "Een mobiel platform spel met verborgen valkuilen";
+    document.getElementById("description4").innerText = "Mijn eerste platform spel met verborgen valkuilen";
+    document.getElementById("description5").innerText = "Vang zo veel mogelijk slakken";
+
+    document.getElementById("link1").href ="pointnl.html";
+    document.getElementById("link2").href ="pongnl.html";
+    document.getElementById("link3").href ="trollnl.html";
+    document.getElementById("link4").href ="trollynl.html";
+    document.getElementById("link5").href ="snailnl.html";
+
+    document.getElementById("invi4").style.display = "flex";
+    document.getElementById("invi5").style.display = "flex";
+}
+
+function website(){
+    document.getElementById("image1").style.backgroundImage = 'url("img/Ameland.png")';
+    document.getElementById("image2").style.backgroundImage = 'url("img/webshop.png")';
+    document.getElementById("image3").style.backgroundImage = 'url("img/portfolio.png")';
+    document.getElementById("image4").style.backgroundImage = '';
+
+    document.getElementById("title1").innerText = "Ameland";
+    document.getElementById("title2").innerText = "Webshop";
+    document.getElementById("title3").innerText = "My first portfolio website";
+    document.getElementById("title4").innerText = "";
+
+    document.getElementById("tags1").innerHTML = "<p>HTML</p>\n"+"<p>CSS</p>";
+    document.getElementById("tags2").innerHTML = "<p>HTML</p>\n" +
+        "                        <p>CSS</p>"+"<p>Javascript</p>";
+    document.getElementById("tags3").innerHTML = "<p>Jimdo</p>";
+    document.getElementById("tags4").innerHTML = "";
+
+    document.getElementById("description1").innerText = "Een website over mijn favoriete eiland";
+    document.getElementById("description2").innerText = "Een oefening om een webshop te leren maken";
+    document.getElementById("description3").innerText = "Mijn eerste portfolio website die ik gemaakt heb om mijzelf te introduceren op Sint Lucas";
+    document.getElementById("description4").innerText = "";
+
+    document.getElementById("link1").href ="amelandnl.html";
+    document.getElementById("link2").href ="webshopnl.html";
+    document.getElementById("link3").href ="portfolionl.html";
+    document.getElementById("link4").href ="";
+    document.getElementById("link5").href ="";
+
+    document.getElementById("invi4").style.display = "none";
+    document.getElementById("invi5").style.display = "none";
+}
+
+function other(){
+    document.getElementById("image1").style.backgroundImage = 'url("img/Judaism.png")';
+    document.getElementById("image2").style.backgroundImage = 'url("img/vijfluik.png")';
+    document.getElementById("image3").style.backgroundImage = 'url("img/creativity.png")';
+    document.getElementById("image4").style.backgroundImage = '';
+
+    document.getElementById("title1").innerText = "Poster over jodendom";
+    document.getElementById("title2").innerText = "5 luik";
+    document.getElementById("title3").innerText = "Professional creativity";
+    document.getElementById("title4").innerText = "";
+
+    document.getElementById("tags1").innerHTML = "<p>Photoshop</p>";
+    document.getElementById("tags2").innerHTML = "<p>C#</p>" +
+        "                        <p>Unity</p>";
+    document.getElementById("tags3").innerHTML = "<p>Adobe premiere pro</p>";
+    document.getElementById("tags4").innerHTML = "";
+
+    document.getElementById("link1").href ="jodendom.html";
+    document.getElementById("link2").href ="luiknl.html";
+    document.getElementById("link3").href ="creativitynl.html";
+    document.getElementById("link4").href ="";
+    document.getElementById("link5").href ="";
+
+    document.getElementById("description1").innerText = "Een poster gemaakt voor burgerschap";
+    document.getElementById("description2").innerText = "Een game over 5 kernwaarden";
+    document.getElementById("description3").innerText = "Een video over wat het betekend om een profesionele ontwerper te worden";
+    document.getElementById("description4").innerText = "";
+
+    document.getElementById("invi4").style.display = "none";
+    document.getElementById("invi5").style.display = "none";
+}
